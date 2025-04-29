af56c0d03e05e1a30f28ac7ae1d2281cadfae039
yogesh jadhav <yogeshinfo1245@gmail.com>
2025-04-29 08:04:04 +0530

    8th

--- src/data/busStops.js
+++ src/data/busStops.js
@@ -1,30 +1,38 @@
 export  const bus101Stops = [
   { lat:23.30171407232595, lng:  77.36203134362793, name: "RGPV" },
-  { lat: 23.273172287208947, lng: 77.36997095849698, name: "LALGHATI SQAURE" },
-  { lat: 23.234247367650024 , lng: 77.4015283292454, name: "New market" },
+  { lat: 23.25341561396029,  lng: 77.3985898196050, name: "KAMLA PARK ROAD" },
   { lat: 23.234247367650024 , lng: 77.4015283292454, name: "New market" },
   { lat: 23.22467133927969,  lng: 77.40208911319817, name: "TULSI NAGAR" },
+  { lat: 23.22117260222136,   lng:77.40370792701171 ,name: "MANIT square" },
   { lat: 23.217883940082572, lng: 77.4056126070792, name: "MANIT GATE" },
 
 ];
 
-  
-  export const bus102Stops = [
-    { lat: 23.217883940082572, lng: 77.4056126070792, name: "MANIT GATE" },
-    { lat: 23.22117260222136,   lng:77.40370792701171 ,name: "MANIT square" },    
-    { lat: 23.22467133927969,  lng: 77.40208911319817, name: "TULSI NAGAR" },
-    { lat:23.226978628481756,  lng:77.40914853627423,   name: "2nd stop"},
-    { lat: 23.23135107787127, lng: 77.41897393824748, name: "SHIVAJI NAGAR" },
-    { lat: 23.231088634906328, lng: 77.43253482077027, name: "MP NAGAR" },
-    { lat: 23.240235261598517, lng: 77.42289717469222, name: "ARERA HILLS" },
-    { lat: 23.252707541322973, lng: 77.41389676964619, name: "JAHANGIRABAD" },
-    { lat: 23.24804148287806, lng: 77.43149047389336, name: "OLD SUBHASH NAGAR" },
-    { lat: 23.251952047973347, lng: 77.43091951938048, name: "NEW SUBHASH NAGAR" },
-    { lat: 23.25910522393857, lng: 77.43149047389336, name: "ASHOKA GARDEN" },
-    { lat: 23.26495162779652, lng: 77.4233056574509, name: "KRISHNA CAMPUS" },
-    { lat: 23.264905602067927, lng: 77.41344757452178, name: "NAVBHAR COLONY" },
-    { lat: 23.267436185272977, lng: 77.41435658883637, name: "BHOPAL JUNCTION" },
+export  const bus102Stops = [
+  { lat:23.30171407232595, lng:  77.36203134362793, name: "RGPV" },
+  { lat: 23.25341561396029,  lng: 77.3985898196050, name: "KAMLA PARK ROAD" },
+  { lat: 23.234247367650024 , lng: 77.4015283292454, name: "New market" },
+  { lat: 23.22467133927969,  lng: 77.40208911319817, name: "TULSI NAGAR" },
+  { lat: 23.22117260222136,   lng:77.40370792701171 ,name: "MANIT square" },
+  { lat: 23.217883940082572, lng: 77.4056126070792, name: "MANIT GATE" },
+
+];
+  // export const bus102Stops = [
+  //   { lat: 23.217883940082572, lng: 77.4056126070792, name: "MANIT GATE" },
+  //   { lat: 23.22117260222136,   lng:77.40370792701171 ,name: "MANIT square" },    
+  //   { lat: 23.22467133927969,  lng: 77.40208911319817, name: "TULSI NAGAR" },
+  //   { lat:23.226978628481756,  lng:77.40914853627423,   name: "2nd stop"},
+  //   { lat: 23.23135107787127, lng: 77.41897393824748, name: "SHIVAJI NAGAR" },
+  //   { lat: 23.231088634906328, lng: 77.43253482077027, name: "MP NAGAR" },
+  //   { lat: 23.240235261598517, lng: 77.42289717469222, name: "ARERA HILLS" },
+  //   { lat: 23.252707541322973, lng: 77.41389676964619, name: "JAHANGIRABAD" },
+  //   { lat: 23.24804148287806, lng: 77.43149047389336, name: "OLD SUBHASH NAGAR" },
+  //   { lat: 23.251952047973347, lng: 77.43091951938048, name: "NEW SUBHASH NAGAR" },
+  //   { lat: 23.25910522393857, lng: 77.43149047389336, name: "ASHOKA GARDEN" },
+  //   { lat: 23.26495162779652, lng: 77.4233056574509, name: "KRISHNA CAMPUS" },
+  //   { lat: 23.264905602067927, lng: 77.41344757452178, name: "NAVBHAR COLONY" },
+  //   { lat: 23.267436185272977, lng: 77.41435658883637, name: "BHOPAL JUNCTION" },
     
-    // Add more...
-  ];
+  //   // Add more...
+  // ];
   
